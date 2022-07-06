631c6be7283f696d2bc731b7abb504cc345eab70
Oliver Abreu <38667404+AbreuOliver@users.noreply.github.com>
2022-07-06 09:29:03 -0400

    added margin above blog title

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -47,8 +47,8 @@
         {/if}
       </div>
       <main
-        class="prose prose-slate prose-sm sm:prose sm:prose-slate sm:prose-lg sm:max-w-none dark:prose-invert flex flex-col w-full flex-grow py-4 px-4"
-      >
+        class="prose prose-slate prose-sm sm:prose sm:prose-slate sm:prose-lg sm:max-w-none dark:prose-invert flex flex-col w-full flex-grow py-4 px-4" style="margin-top: 0.5rem"
+      > 
         <slot />
       </main>
     </div>
