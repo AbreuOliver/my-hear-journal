58676bc731ab7c0d359c87c54c65562e123d508c
Oliver Abreu <38667404+AbreuOliver@users.noreply.github.com>
2022-07-07 23:18:00 -0400

    added additional component

---
+++ src/lib/components/Intro.svelte
@@ -0,0 +1,4 @@
+<div>
+    <strong style="font-weight: 800; font-size: clamp(1.42em, 1.5em, 1.67em); line-height: 1.3333333">Today's Scripture Reading</strong><br/><br/>
+    <strong style="font-weight: 700; color: inherit; font-size: clamp(1.42em, 1.5em, 1.67em); line-height: 1.5"><slot></slot></strong>
+</div>
