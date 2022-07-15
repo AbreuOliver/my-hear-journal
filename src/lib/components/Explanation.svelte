41f201da27765de7268cfcd8607af5bb4b544056
Oliver Abreu <38667404+AbreuOliver@users.noreply.github.com>
2022-07-15 12:18:32 -0400

    tests to Explanation

--- src/lib/components/Explanation.svelte
+++ src/lib/components/Explanation.svelte
@@ -6,11 +6,18 @@
 
 <div>
     <h2>Explanation</h2>
-        <h3>Context of Passage</h3>
-            <blockquote style="font-weight: bold !important">
+          <strong 
+        style="font-weight: 800; font-size: clamp(1.42em, 1.5em, 1.67em); line-height: 1.3333333"
+    >Context of Passage</strong>
+    <br/>
+    <br/>
+            <blockquote style="font-weight: bold 
+            !important">
                 <p>{passageContext}</p>
             </blockquote>
-        <h3>Key Word(s) of Passage<h3>
+          <strong 
+        style="font-weight: 800; font-size: clamp(1.42em, 1.5em, 1.67em); line-height: 1.3333333"
+    >Key Word(s) of Passage</strong>
             <p><slot></slot></p>
         <h3>My Summary of Highlight<h3>
             <p>{mySummary}</p>
