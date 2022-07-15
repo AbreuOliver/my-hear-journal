26f01e3ec30f3f41f75bcae18833305d99f00a7d
Oliver Abreu <38667404+AbreuOliver@users.noreply.github.com>
2022-07-15 11:57:37 -0400

    testing further changes

--- src/lib/components/Explanation.svelte
+++ src/lib/components/Explanation.svelte
@@ -1,18 +1,15 @@
 <script>
 	import Gradient from './Gradient.svelte';
     export let passageContext = '';
+    export let mySummary = '';
 </script>
 
 <div>
-## **Explanation**
-
-### **Context of Passage**
-
-    > {passageContext}
-
-### **Key Word of Highlight**
-
-    
-
-    Biblical repentance is a laborious task that involves not only a mental and emotional response but should .
+    <h2>Explanation</h2>
+        <h3>Context of Passage</h3>
+            <blockquote>{passageContext}</blockquote>
+        <h3>Key Word(s) of Passage<h3>
+            <slot></slot>
+        <h3>My Summary of Highlight<h3>
+            <p>{mySummary}</p>
 </div>
