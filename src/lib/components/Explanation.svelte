2288bb806d4e96e98f446386736c8d8e1aa273ea
Oliver Abreu <38667404+AbreuOliver@users.noreply.github.com>
2022-07-15 13:33:14 -0400

    several changes

--- src/lib/components/Explanation.svelte
+++ src/lib/components/Explanation.svelte
@@ -10,18 +10,22 @@
             <strong>Explanation</strong>
         </a>
     </h2>
-        <h3 id="passageContext">
-            <a href="#passageContext">
-                <strong>Passage Context</strong>
-            </a>
-        </h3>
-            <p>{passageContext}</p>
-        <h3 id="passageKeyTerms">
-            <a href="#passageKeyTerms">
-                <strong>Passage Key Terms</strong>
-            </a>
-        </h3>
-            <p><slot></slot></p>
-        <h3>My Summary of Highlight<h3>
-        <p>{mySummary}</p>
+    <h3 id="passageContext">
+        <a href="#passageContext">
+            <strong>Passage Context</strong>
+        </a>
+    </h3>
+        <p>{passageContext}</p>
+    <h3 id="passageKeyTerms">
+        <a href="#passageKeyTerms">
+            <strong>Passage Key Terms</strong>
+        </a>
+    </h3>
+        <p><slot></slot></p>
+    <h3 id="mySummary">
+        <a href="#mySummary">
+            <strong>My Summary</strong>
+        </a>
+    </h3>
+    <p>{mySummary}</p>
 </div>
