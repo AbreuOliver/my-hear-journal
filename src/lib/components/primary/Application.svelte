a49454c9b5266ab6aa60eba12f5f74e2a3bfcf7b
Oliver Abreu <38667404+AbreuOliver@users.noreply.github.com>
2022-07-15 13:41:30 -0400

    added Application component

---
+++ src/lib/components/primary/Application.svelte
@@ -0,0 +1,25 @@
+<script>
+	import Gradient from './Gradient.svelte';
+    export let historicalApplication = '';
+    export let contemporaryApplication = '';
+</script>
+
+<div>
+    <h2 id="application">
+        <a href="#application">
+            <strong>Application</strong>
+        </a>
+    </h2>
+    <h3 id="historicalApplication">
+        <a href="#historicalApplication">
+            <strong>Historical Application</strong>
+        </a>
+    </h3>
+        <p>{historicalApplication}</p>
+    <h3 id="contemporaryApplication">
+        <a href="#contemporaryApplication">
+            <strong>Contemporary Application</strong>
+        </a>
+    </h3>
+    <p>{contemporaryApplication}</p>
+</div>
