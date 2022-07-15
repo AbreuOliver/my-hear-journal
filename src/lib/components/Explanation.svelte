48708a3f89f3e11d8c8ce601b008a04453dc0676
Oliver Abreu <38667404+AbreuOliver@users.noreply.github.com>
2022-07-15 15:36:30 -0400

    testing conditional rendering

--- src/lib/components/Explanation.svelte
+++ src/lib/components/Explanation.svelte
@@ -21,10 +21,10 @@
             <p>{passageContext}</p>
             
         </blockquote>
-        <!-- {#if citationURL != " "} -->
-        <a href="{citationURL}">{citation}</a>
-        <!-- {/if}
-        {#if citationURL == " "}
+        {#if citationURL != " "}
+        <a href="{citationURL}"> - {citation}</a>
+        {/if}
+        <!-- {#if citationURL == " "}
         {/if} -->
     <h3 id="passageKeyTerms">
         <a href="#passageKeyTerms">
