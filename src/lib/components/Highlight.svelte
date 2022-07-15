667622882e8e21bf70fabd2885f7827d05debed6
Oliver Abreu <38667404+AbreuOliver@users.noreply.github.com>
2022-07-15 13:00:11 -0400

    should correct Highlight component

--- src/lib/components/Highlight.svelte
+++ src/lib/components/Highlight.svelte
@@ -5,7 +5,13 @@
 </script>
 
 <div>
-    <h2 id="highlight"><a href="highlight"><strong>Highlight</a></strong></h2>
+    <h2 id="highlight">
+        <a href="#highlight">
+            <strong>
+                Highlight
+            </strong>
+        </a>
+    </h2>
 
     <blockquote style="font-weight: bold !important"> 
     <Gradient>"{verseText}"</Gradient>
