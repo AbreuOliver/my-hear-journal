34e0b3e356037d2b98654d4606b1d692ff5f514c
Oliver Abreu <38667404+AbreuOliver@users.noreply.github.com>
2022-07-15 18:15:35 -0400

    changed index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,6 +8,23 @@
       }
     }
   }
+
+  let currentdate = new Date()
+  var oneJan = new Date(currentdate.getFullYear(), 0, 1)
+  var numberOfDays = Math.floor((currentdate - oneJan) / (24 * 60 * 60 * 1000))
+  var result = Math.ceil((currentdate.getDay() + 1 + numberOfDays) / 7)
+
+  const dayOfWeekDigit = new Date().getDay()
+  console.log(dayOfWeekDigit) // 👉️ 0
+
+  let dayOfWeekName = new Date().toLocaleString('default', { weekday: 'long' })
+  console.log('Day of Week Name:', dayOfWeekName)
+  let readingStatus = ''
+  if ((dayOfWeekName = 'Friday')) {
+    readingStatus = 'Finished reading '
+  } else {
+    readingStatus = 'Currently reading '
+  }
 </script>
 
 <script>
@@ -24,25 +41,50 @@
 
 <div class="flex flex-col flex-grow">
   <!-- replace with a bio about you, or something -->
-  <div class="flex items-center justify-center text-xl h-40">
-    <!-- <ButtonLink size="large" href="https://github.com/mattjennings/sveltekit-blog-template">
+  <div class="flex flex-col items-start justify-start text-xl h-full">
+    <h1 class="pt-6">
+      <span
+        class="!text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-teal-500 dark:from-violet-500 dark:to-pink-500"
+        >Study</span
+      > to show thyself approved unto God
+    </h1>
+    <h4 style="color: darkgray">
+      Observations and study notes from the F260 Bible reading plan. <br />
+      <strong>Current Reading Status:</strong> <br />
+      {readingStatus} plan for
+      <strong
+        ><span
+          class="!text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-teal-500 dark:from-violet-500 dark:to-pink-500"
+          >Week {result - 1}</span
+        ></strong
+      >.
+    </h4>
+    <br />
+    <ButtonLink
+      size="large"
+      target="_blank"
+      href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&ved=2ahUKEwjQrsCR8vv4AhUMGFkFHfrJD1QQFnoECBAQAQ&url=https%3A%2F%2Freplicate.org%2Fwp-content%2Fuploads%2F2014%2F12%2FF260-Reading-Plan-2018.pdf&usg=AOvVaw2NJGxjsgRqKjhjmO8GLzhl"
+    >
       <slot slot="icon-start">
         <svg
-          class="fill-black dark:fill-white h-6 w-6"
+          class="fill-white dark:fill-dark h-6 w-6"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
-        >
-          <path
-            d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.5,4.7,2.2,8.9,6.3,10.5C8.7,21.4,9,21.2,9,20.8v-1.6c0,0-0.4,0.1-0.9,0.1 c-1.4,0-2-1.2-2.1-1.9c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1 c0.4,0,0.7-0.1,0.9-0.2c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6C7,7.2,7,6.6,7.3,6 c0,0,1.4,0,2.8,1.3C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3C15.3,6,16.8,6,16.8,6C17,6.6,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4 c0.7,0.8,1.2,1.8,1.2,3c0,2.2-1.7,3.5-4,4c0.6,0.5,1,1.4,1,2.3v2.6c0,0.3,0.3,0.6,0.7,0.5c3.7-1.5,6.3-5.1,6.3-9.3 C22,6.1,16.9,1.4,10.9,2.1z"
+          width="24"
+          height="24"
+          fill="none"
+          stroke="#000000"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          ><path d="M14 2H6a2 2 0 0 0-2 2v16c0 1.1.9 2 2 2h12a2 2 0 0 0 2-2V8l-6-6z" /><path
+            d="M14 3v5h5M16 13H8M16 17H8M10 9H8"
           /></svg
         >
       </slot>
-      View on GitHub
+      View the F260 reading plan (PDF)
       <slot slot="icon-end" />
-    </ButtonLink> -->
-    <h1 class="pt-6"><span class="!text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-teal-500 dark:from-violet-500 dark:to-pink-500">Study</span> to show thyself approved unto God</h1>
-    ---
-    <h4>The observations and study notes of Oliver Abreu.</h4>
+    </ButtonLink>
   </div>
 
   <!-- recent posts -->
