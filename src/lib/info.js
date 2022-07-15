57171919cfe758a2d688a8e6cc04654a840af475
Oliver Abreu <38667404+AbreuOliver@users.noreply.github.com>
2022-07-15 16:03:35 -0400

    aesthetic changes

--- src/lib/info.js
+++ src/lib/info.js
@@ -5,7 +5,7 @@
 /**
  * Used for the header of the page and in SEO
  */
-export const name = `HEAR Journal` // replace with your name
+export const name = `Oliver's HEAR Journal` // replace with your name
 
 /**
  * Used for SEO in /posts/[slug]
