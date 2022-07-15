e072cef3b5a02451d377becdebbdc138356a957b
Oliver Abreu <38667404+AbreuOliver@users.noreply.github.com>
2022-07-15 14:41:17 -0400

    testing Response component

---
+++ src/lib/components/primary/Response.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let myPrayer = '';
+</script>
+
+<div>
+    <h2 id="response">
+        <a href="#response">
+            <strong>Response</strong>
+        </a>
+    </h2>
+    <p>Heavenly Father,</p> <br/>
+    <p>{myPrayer}</p> <br/>
+    <p>Amen</p>
+
+</div>
