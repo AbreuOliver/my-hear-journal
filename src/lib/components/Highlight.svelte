e25b49f1f437b6169195a4a24741f1e2cbf6af08
Oliver Abreu <38667404+AbreuOliver@users.noreply.github.com>
2022-07-15 10:39:02 -0400

    testing Highlight component

---
+++ src/lib/components/Highlight.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Gradient from './Gradient.svelte';
+    export let passage = '';
+    export let reference = '';
+</script>
+
+<div>
+    <h2>Highlight</h2>
+
+    <blockquote style="font-weight: bold !important"> 
+    <Gradient>"{passage}"</Gradient>
+    </blockquote> - {reference}
+</div>
