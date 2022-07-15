357574126957243235491632d350c8836f0b92a8
Oliver Abreu <38667404+AbreuOliver@users.noreply.github.com>
2022-07-15 10:27:14 -0400

    testing TodayPassage component

---
+++ src/lib/components/TodayPassage.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Gradient from './Gradient.svelte';
+    export let passage = '';
+</script>
+
+<div>
+    <strong 
+        style="font-weight: 800; font-size: clamp(1.42em, 1.5em, 1.67em); line-height: 1.3333333"
+    >
+        Today's Scripture Reading
+    </strong>
+    <br/>
+    <br/>
+    <strong 
+        style="font-weight: 700; color: inherit; font-size: clamp(1.42em, 1.5em, 1.67em); line-height: 1.5"
+    >
+        <Gradient>
+            {passage}
+        </Gradient>
+    </strong>
+</div>
